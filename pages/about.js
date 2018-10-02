d10a1fbfcb30fbd20e259c42679989ae676e094d
sophialittlejohn <littlejohn.sophia@gmail.com>
2018-10-02 10:48:09 +0200

    End of traversyMedia tutorial

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from "../components/Layout";
+
+const About = () => (
+    <Layout>
+        <div>
+            <h1>About Prices!</h1>
+            <p>Application to view Bitcoin prices</p>
+        </div>
+    </Layout>
+)
+
+export default About
